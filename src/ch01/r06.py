77be0d341030019872b7f4cba74fe382a231fb9b
Son-Huy TRAN <sonhuytran@gmail.com>
2013-08-05 17:50:05 +0200

    r06

---
+++ src/ch01/r06.py
@@ -0,0 +1,11 @@
+'''
+Created on 5 août 2013
+
+@author: Son-Huy TRAN
+'''
+
+def sum_odd_squares(n):
+    return sum([k * k for k in range(1, n) if (k % 2 == 1)]);
+
+if __name__ == '__main__':
+    print(sum_odd_squares(10));
