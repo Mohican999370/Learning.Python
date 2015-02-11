fabb56ddf574567767a12961395ca6e713847668
sonhuytran <sonhuytran@gmail.com>
2015-02-11 17:45:09 +0100

    PyGTK3: HelloWorld

--- PyGtk3/01helloworld.py
+++ PyGtk3/01helloworld.py
@@ -4,9 +4,25 @@ __email__ = "sonhuytran@gmail.com"
 __doc__ = ''
 __version__ = '1.0'
 
-from gi.repository import Gtk
+from gi.repository import Gtk, Gdk
 
-win = Gtk.Window()
+
+class MyWindow(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self, title="Hello World", type=Gtk.WindowType.TOPLEVEL)
+        self.set_position(Gtk.WindowPosition.CENTER)
+        self.stick()
+
+        self.button = Gtk.Button(label="Click Here")
+        self.button.connect("clicked", self.on_button_clicked)
+        self.add(self.button)
+
+    def on_button_clicked(self, widget):
+        print("Hello World")
+
+
+win = MyWindow()
 win.connect("delete-event", Gtk.main_quit)
+win.set_resizable(True)
 win.show_all()
 Gtk.main()
